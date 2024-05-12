0882b956990890f1034dd54213f3861f400d194d
mizael-anthony <mizaelanthony07@gmail.com>
2024-05-12 10:52:47 +0300

    clean all

--- db/migrate/20230624070222_add_slug_to_category.rb
+++
@@ -1,7 +0,0 @@
-class AddSlugToCategory < ActiveRecord::Migration[7.0]
-  def change
-    change_table :categories do |t|
-      t.string :slug
-    end
-  end
-end
--- test/controllers/posts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get private" do
-    get posts_private_url
-    assert_response :success
-  end
-end
